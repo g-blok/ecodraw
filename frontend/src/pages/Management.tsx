921459b3e9de23adfd262bec07c45ec42c07f5aa
g-blok <gwblocher@gmail.com>
2024-08-29 07:36:50 -0700

    improved management page

--- frontend/src/pages/Management.tsx
+++ frontend/src/pages/Management.tsx
@@ -7,6 +7,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Device } from '../common/types/types';
+import { COST_MULTIPLIERS, SPACING } from '../common/constants/constants';
 import { getDefaultDevices } from '../services/apiService';
 import { debounce } from 'lodash';
 import { numberToString, numberToMoneyString } from '../utils/formatUtils'
@@ -50,39 +51,100 @@ const ManagementPage = () => {
 				/>
 			</div>
 		) : (
-			<div className='w-2/3'>
-				<div className='font-bold text-lg'>Devices</div>
-				<TableContainer component={Paper}>
-				<Table>
-					<TableHead>
-					<TableRow>
-						<TableCell>Name</TableCell>
-						<TableCell align='center'>Manufacturer</TableCell>
-						<TableCell align='right'>Length (ft)</TableCell>
-						<TableCell align='right'>Width (ft)</TableCell>
-						<TableCell align='right'>Cost</TableCell>
-						<TableCell align='right'>Capacity (kWh)</TableCell>
-						<TableCell align='right'>Release Date</TableCell>
-					</TableRow>
-					</TableHead>
-					<TableBody>
-					{defaultDevices.map((row, index) => (
-						<TableRow
-							key={index}
-							className={`${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}`}
-						>
-							<TableCell>{row.name}</TableCell>
-							<TableCell align='center'>{row.mfg}</TableCell>
-							<TableCell align='right'>{row.length}</TableCell>
-							<TableCell align='right'>{row.width}</TableCell>
-							<TableCell align='right'>{numberToMoneyString(row.cost)}</TableCell>
-							<TableCell align='right'>{numberToString(row.capacity_kwh)}</TableCell>
-							<TableCell align='right'>{row.release_date}</TableCell>
-						</TableRow>
-					))}
-					</TableBody>
-				</Table>
-				</TableContainer>
+			<div className='flex flex-col w-2/3 gap-16'>
+				<div>
+					<div className='font-bold text-lg'>Devices</div>
+					<TableContainer component={Paper}>
+						<Table>
+							<TableHead>
+							<TableRow>
+								<TableCell>Name</TableCell>
+								<TableCell align='center'>Manufacturer</TableCell>
+								<TableCell align='right'>Length (ft)</TableCell>
+								<TableCell align='right'>Width (ft)</TableCell>
+								<TableCell align='right'>Cost</TableCell>
+								<TableCell align='right'>Capacity (kWh)</TableCell>
+								<TableCell align='right'>Release Date</TableCell>
+							</TableRow>
+							</TableHead>
+							<TableBody>
+							{defaultDevices.map((row, index) => (
+								<TableRow
+									key={index}
+									className={`${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}`}
+								>
+									<TableCell>{row.name}</TableCell>
+									<TableCell align='center'>{row.mfg}</TableCell>
+									<TableCell align='right'>{row.width}</TableCell>
+									<TableCell align='right'>{row.length}</TableCell>
+									<TableCell align='right'>{numberToMoneyString(row.cost)}</TableCell>
+									<TableCell align='right'>{numberToString(row.capacity_kwh)}</TableCell>
+									<TableCell align='right'>{row.release_date}</TableCell>
+								</TableRow>
+							))}
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</div>
+				<div>
+					<div className='font-bold text-lg'>Costs</div>
+					<TableContainer component={Paper}>
+						<Table>
+							<TableHead>
+							<TableRow>
+								<TableCell>Name</TableCell>
+								<TableCell align='center'>Id</TableCell>
+								<TableCell align='right'>Sort</TableCell>
+								<TableCell align='right'>Type</TableCell>
+								<TableCell align='right'>Multiplier</TableCell>
+							</TableRow>
+							</TableHead>
+							<TableBody>
+							{COST_MULTIPLIERS.map((row, index) => (
+								<TableRow
+									key={index}
+									className={`${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}`}
+								>
+									<TableCell>{row.display}</TableCell>
+									<TableCell align='center'>{row.name}</TableCell>
+									<TableCell align='right'>{row.sort}</TableCell>
+									<TableCell align='right'>{row.multiplier_type}</TableCell>
+									<TableCell align='right'>{row.multiplier}</TableCell>
+								</TableRow>
+							))}
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</div>
+				<div>
+					<div className='font-bold text-lg'>Spacing</div>
+					<div className='flex flex-col gap-2 w-96 mt-2 ml-2'>
+						<div className='flex justify-between'>
+							<div>Walkway Width</div>
+							<div className='font-bold'>{SPACING.WALKWAY_WIDTH} ft</div>
+						</div>
+						<div className='flex justify-between'>
+							<div>Transformer Gap</div>
+							<div className='font-bold'>{SPACING.TRANSFORMER_GAP} ft</div>
+						</div>
+						<div className='flex justify-between'>
+							<div>Storage Gap</div>
+							<div className='font-bold'>{SPACING.STORAGE_GAP} ft</div>
+						</div>
+						<div className='flex justify-between'>
+							<div>Max Device Width</div>
+							<div className='font-bold'>{SPACING.MAX_DEVICE_WIDTH} ft</div>
+						</div>
+						<div className='flex justify-between'>
+							<div>Max System Width</div>
+							<div className='font-bold'>{SPACING.MAX_SYSTEM_WIDTH} ft</div>
+						</div>
+						<div className='flex justify-between'>
+							<div>Outer Offset</div>
+							<div className='font-bold'>{SPACING.OUTER_OFFSET} ft</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		)}
     </div>
