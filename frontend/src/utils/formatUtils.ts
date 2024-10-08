9d1041ab30e283c74b405e1d96312a0234646422
g-blok <gwblocher@gmail.com>
2024-10-07 20:31:05 -0700

    added toTitleCase util

--- frontend/src/utils/formatUtils.ts
+++ frontend/src/utils/formatUtils.ts
@@ -6,3 +6,10 @@ export const numberToMoneyString = (value: number): string => {
     const valueString = value.toLocaleString('en-US', {maximumFractionDigits:2});
     return value < 0 ? '-$'.concat(valueString) : '$'.concat(valueString);
 }
+
+
+export const toTitleCase = (str: string) => {
+    return str.toLowerCase().split(' ').map((word: string) => {
+        return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join(' ');
+}
